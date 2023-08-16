e1af5f1ea891a437100cfb2e88ed8792f4870054
TonyITman <119666149+TonyITman@users.noreply.github.com>
2023-08-16 11:25:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+window.onload=function(){
+    setTimeout(function(){
+        document.querySelector('.preloader').style.display='none';
+        document.querySelector('.loaders').style.display='block';
+
+        window.location.hre='index.html';
+    },4000);
+};
